f663d9057625517a637c67806751e03bfb88dfb7
CAMUSIN <fergg8704@gmail.com>
2023-07-01 00:20:34 -0600

    Add: constants,square

--- 01-tic-tac-toe/src/App.jsx
+++ 01-tic-tac-toe/src/App.jsx
@@ -1,30 +1,107 @@
 import { useState } from 'react'
 import './App.css'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square.jsx'
+import { TURNS, WINNING_COMBOS } from './constants.js'
 
 function App() {
-  const TURNS = {
-    X: 'x',
-    O: 'o'
+  
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    //revisamos combinaciones ganadoras
+    for(const combo of WINNING_COMBOS) {
+      const [a, b, c] = combo
+      if(boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    //si no hay ganador, devolvemos null
+    return null
   }
 
-  const board = Array(9).fill(null)
+
+  const updateBoard = (index) => {
+    //No sobre escribe celda
+    if(board[index] || winner) return
+    
+    //actualiza el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    
+    //actualiza el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    //comprueba ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      confetti()
+      setWinner(newWinner)
+    } else if(checkEndGame(newBoard)){
+      setWinner(false)
+    }
+  }
+
+  const checkEndGame = (boardToCheck) => {
+    return boardToCheck.every(cell => cell !== null) && winner === null
+  }
+
+  const handleReset = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
+      <button onClick={handleReset}>Reset Juego</button>
       <section className='game'>
         {
           board.map((_, index) => {
             return (
-              <div className='cell' key={index}>
-                <span className='cell__content'>
-                  {index}
-                </span>
-              </div>
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {board[index]}
+              </Square>
             )
           })
         }
       </section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>X</Square>
+        <Square isSelected={turn === TURNS.O}>O</Square>
+      </section>
+
+      {
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner === false 
+                    ? 'Empate' 
+                    : 'Ganador:'
+                }
+              </h2>
+              <header className='win'>
+                { winner && <Square>{winner}</Square>}
+              </header>
+              <footer>
+                <button onClick={handleReset}>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }
